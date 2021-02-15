22a77a0a1b5127577f096a7c514d114bdfd56813
mehmetvural2020 <mehmetvurall@gmail.com>
2021-02-16 02:21:00 +0700

    Runner.java file created.

---
+++ src/test/java/org/cb/contextCustom/runner/Runner.java
@@ -0,0 +1,21 @@
+package org.cb.contextCustom.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        // Glue path must start after java folder. (after this -> src/test/java)
+        glue = "org/cb/contextCustom/stepdef",
+        dryRun =false,
+        strict = true,
+//        tags = "@smoke_test,@unit_test",
+        plugin = {"html:target/default-cucumber-reports",
+                "json:target/cucumber.json"
+        }
+)
+public class Runner {
+
+}
