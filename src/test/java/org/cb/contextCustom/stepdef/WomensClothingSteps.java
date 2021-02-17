dd06089b4e3691b3c9cc18018324c353cf57e42c
mehmetvural2020 <mehmetvurall@gmail.com>
2021-02-17 17:30:54 +0700

    I created WomensClothing.feature file, WomensClothingSteps step and WomensClothing classes.

---
+++ src/test/java/org/cb/contextCustom/stepdef/WomensClothingSteps.java
@@ -0,0 +1,41 @@
+package org.cb.contextCustom.stepdef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.cb.contextCustom.pages.WomensClothing;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WomensClothingSteps extends Base{
+    WomensClothing womensClothing = new WomensClothing();
+    Logger logger = LoggerFactory.getLogger(WomensClothingSteps.class);
+
+    @When("move on to Categories dropdown menu")
+    public void moveOnToCategoriesDropdownMenu() {
+        moveToElement(womensClothing.categories);
+    }
+
+    @Then("move on to Women's Clothing sub-menu")
+    public void moveOnToWomenSClothingSubMenu() {
+        moveToElement(womensClothing.womenscloth);
+    }
+
+    @And("move and click on All Women's Clothing")
+    public void moveAndClickOnAllWomenSClothing() {
+        moveAndClickToElement(womensClothing.allWomen);
+    }
+
+    @Then("verify that Women's Clothing text is present")
+    public void verifyThatWomenSClothingTextIsPresent() {
+        String expected = "Women's Clothing";
+        String actual = womensClothing.pageMapText.getText();
+        verifyTheText(actual, expected);
+//        boolean verifyText = verifyTheText(actual,expected);
+//        if(verifyText){
+//            logger.info("{} text is displayed.",actual);
+//        }else logger.info("{} is not displayed.",expected);
+    }
+}
