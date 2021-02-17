f3fe79a0b55c82305e41fb6019d4b6c699fc33a0
Esizsu <esra_izgi@hotmail.com>
2021-02-17 11:45:08 -0800

    Created MainPage1.feature, MainPage1.java and MainPageSteps1

--- src/test/java/org/cb/contextCustom/pages/MainPage1.java
+++ src/test/java/org/cb/contextCustom/pages/MainPage1.java
@@ -1,6 +1,8 @@
 package org.cb.contextCustom.pages;
 
 import org.cb.contextCustom.utils.MyDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class MainPage1 {
@@ -8,6 +10,69 @@ public class MainPage1 {
     public MainPage1() {
         PageFactory.initElements(MyDriver.get(), this);
     }
+
+    @FindBy(xpath = "//a[@class='btn btn-outline-accent']")
+    public WebElement moreProductsButton;
+
+    @FindBy(xpath = "//img[@alt='accessories']")
+    public WebElement accessoriesPicture;
+
+    @FindBy(xpath = "//h2[@class='h5']//a[contains(text(), 'Accessories')]")
+    public WebElement accessories;
+
+    @FindBy(xpath = "//a[@class='nav-link-style'][normalize-space()='Backpacks']")
+    public WebElement backpacks;
+
+    @FindBy(css = "img[alt='hats']")
+    public WebElement hatPicture;
+
+    @FindBy(xpath = "//h2[@class='h5']//a[contains(text(),'Hats')]")
+    public WebElement hats;
+
+    @FindBy (xpath = "//a[@class='nav-link-style'][normalize-space()='Beanies']")
+    public WebElement beanies;
+
+    @FindBy (xpath = "//img[@alt='home-living']")
+    public WebElement homeLivingPicture;
+
+    @FindBy (xpath = "//h2[@class='h5']//a[contains(text(),'Home & Living')]" )
+    public WebElement homeLiving;
+
+    @FindBy (xpath = "//a[@class='nav-link-style'][normalize-space()='Blankets']" )
+    public WebElement blankets;
+
+    @FindBy (xpath = "//img[@alt='kids-youth-clothing']]")
+    public WebElement kidsYouthPicture;
+
+    @FindBy (xpath = "//h2[@class='h5']//a[contains(text(),'Kids & Youth Clothing')]")
+    public WebElement kidsYouthClothing;
+
+    @FindBy (xpath = "//a[@class='nav-link-style'][contains(text(),'Baby Bibs')]")
+    public WebElement babybibs;
+
+    @FindBy (xpath = "//img[@alt='mens-clothing']")
+    public WebElement mensClothingPicture;
+
+    @FindBy (xpath = "//div[5]//div[1]//div[1]//h2[1]//a[1]")
+    public WebElement mensClothing;
+
+    @FindBy (xpath = "//div[5]//div[1]//div[1]//ul[1]//li[1]//a[1]")
+    public WebElement Bottoms;
+
+    @FindBy (xpath = "//img[@alt='womens-clothing']")
+    public WebElement womensClothingPicture;
+
+    @FindBy (xpath = "//div[6]//div[1]//div[1]//h2[1]//a[1]")
+    public WebElement womensClothing;
+
+    @FindBy (xpath = "//a[@class='nav-link-style'][normalize-space()='Crop Tops']")
+    public WebElement cropTops;
+
+
+
+
+
+
 
 
 }
--- src/test/java/org/cb/contextCustom/stepdef/MainPageSteps1.java
+++ src/test/java/org/cb/contextCustom/stepdef/MainPageSteps1.java
@@ -3,52 +3,91 @@ package org.cb.contextCustom.stepdef;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.cb.contextCustom.pages.MainPage1;
+import org.cb.contextCustom.utils.ConfigurationReader;
+import org.cb.contextCustom.utils.MyDriver;
+import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MainPageSteps1 extends Base{
+public class MainPageSteps1 extends Base {
 
     MainPage1 mainPage1 = new MainPage1();
-    Logger logger = LoggerFactory.getLogger(MainPageSteps.class);
+    Logger logger = LoggerFactory.getLogger(MainPageSteps1.class);
 
     @Given("User is on landing page")
     public void userIsOnLandingPage() {
+      //  MyDriver.get().get(ConfigurationReader.getProperty("baseUrl"));
+        String expected = "Context Custom";
+        String actual = MyDriver.get().getTitle();
+        Assert.assertEquals(expected, actual);
+
+        logger.info("contextcustom.com website title is {}", actual);
     }
 
     @Then("User verifies that More products button is present")
     public void userVerifiesThatMoreProductsButtonIsPresent() {
+        mainPage1.moreProductsButton.isDisplayed();
+        logger.info("More products button is displayed.");
+
     }
 
-    @Then("User verifies that {string} and {string} are present")
-    public void userVerifiesThatAndArePresent(String arg0, String arg1) {
+    @Then("User verifies that {string} is present")
+    public void userVerifiesThatIsPresent(String value) {
+
+        String actual;
+        if(value.equals(mainPage1.accessories.getText())){
+            actual = mainPage1.accessories.getText();
+            Assert.assertEquals(value,actual);
+            logger.info("{} is verified", value);
+
+//        }else if(mainPage1.accessoriesPicture.isDisplayed()){
+//            logger.info("{} is verified", value);
+
+        }else if(value.equals(mainPage1.hatPicture.getText())) {
+            actual = mainPage1.hatPicture.getText();
+            Assert.assertEquals(value, actual);
+            logger.info("{} is verified", value);
+
+        } else if(value.equals(mainPage1.hats.getText())) {
+            actual = mainPage1.hats.getText();
+            Assert.assertEquals(value, actual);
+            logger.info("{} is verified", value);
+        }
+
+        else{
+            logger.warn("Failed");
+        }
+
     }
 
-    @Then("User verifies that {string} under Accessories is present")
-    public void userVerifiesThatUnderAccessoriesIsPresent(String arg0) {
-    }
 
-    @Then("User verifies that {string} under Hat is present")
-    public void userVerifiesThatUnderHatIsPresent(String arg0) {
-    }
+//        mainPage1.accessoriesPicture.isDisplayed();
 
-    @Then("User verifies that {string} under Home & Living is present")
-    public void userVerifiesThatUnderHomeLivingIsPresent(String arg0) {
-    }
-
-    @Then("User verifies that {string} under Kids & Youth Clothing is present")
-    public void userVerifiesThatUnderKidsYouthClothingIsPresent(String arg0) {
-    }
-
-    @Then("User verifies that {string} under Men's Clothing is present")
-    public void userVerifiesThatUnderMenSClothingIsPresent(String arg0) {
-    }
-
-//    @Then("User verifies that {string}and {string} are present")
-//    public void userVerifiesThatAndArePresent(String arg0, String arg1) {
 //    }
 
-    @Then("User verifies that {string} under Women's Clothing is present")
-    public void userVerifiesThatUnderWomenSClothingIsPresent(String arg0) {
+
+    @Then("User verifies that Backpacks under Accessories is present")
+    public void userVerifiesThatBackpacksUnderAccessoriesIsPresent() {
+    }
+
+    @Then("User verifies that Beanies under Hat is present")
+    public void userVerifiesThatBeaniesUnderHatIsPresent() {
+    }
+
+    @Then("User verifies that Blankets under Home & Living is present")
+    public void userVerifiesThatBlanketsUnderHomeLivingIsPresent() {
+    }
+
+    @Then("User verifies that Baby Bibs under Kids & Youth Clothing is present")
+    public void userVerifiesThatBabyBibsUnderKidsYouthClothingIsPresent() {
+    }
+
+    @Then("User verifies that Bottoms under Men's Clothing is present")
+    public void userVerifiesThatBottomsUnderMenSClothingIsPresent() {
+    }
+
+    @Then("User verifies that Crop Tops under Women's Clothing is present")
+    public void userVerifiesThatCropTopsUnderWomenSClothingIsPresent() {
     }
 
 
