53ea2de0b95a922b2e870a73a06881ef958401af
Esizsu <esra_izgi@hotmail.com>
2021-02-18 23:34:58 -0800

    Renamed MainPage1 as MoreProducts and modified MoreProductsPage, MoreProducts.feature and MoreProductsSteps

---
+++ src/test/java/org/cb/contextCustom/pages/MoreProductsPage.java
@@ -0,0 +1,81 @@
+package org.cb.contextCustom.pages;
+
+import org.cb.contextCustom.utils.MyDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MoreProductsPage {
+
+    public MoreProductsPage() {
+        PageFactory.initElements(MyDriver.get(), this);
+    }
+
+    @FindBy(xpath = "//a[@class='btn btn-outline-accent']")
+    public WebElement moreProductsButton;
+
+    @FindBy(xpath = "//img[@alt='accessories']")
+    public WebElement accessoriesImage;
+
+    @FindBy(xpath = "//h2[@class='h5']//a[contains(text(), 'Accessories')]")
+    public WebElement accessories;
+
+    @FindBy(xpath = "//a[@class='nav-link-style'][normalize-space()='Backpacks']")
+    public WebElement backpacks;
+
+    @FindBy(css = "img[alt='hats']")
+    public WebElement hatImage;
+
+    @FindBy(xpath = "//h2[@class='h5']//a[contains(text(),'Hats')]")
+    public WebElement hats;
+
+    @FindBy (xpath = "//a[@class='nav-link-style'][normalize-space()='Beanies']")
+    public WebElement beanies;
+
+    @FindBy (xpath = "//img[@alt='home-living']")
+    public WebElement homeLivingImage;
+
+    @FindBy (xpath = "//h2[@class='h5']//a[contains(text(),'Home & Living')]" )
+    public WebElement homeLiving;
+
+    @FindBy (xpath = "//a[@class='nav-link-style'][normalize-space()='Blankets']" )
+    public WebElement blankets;
+
+    @FindBy (xpath = "//img[@alt='kids-youth-clothing']")   // buradaki attribute gore yani alt'in karsiligi olan kids-youth-clothing'i feature file da vermek lazim ki buna gore kiyas yapacaz
+    public WebElement kidsYouthImage;
+
+    @FindBy (xpath = "//h2[@class='h5']//a[contains(text(),'Kids & Youth Clothing')]")
+    public WebElement kidsYouthClothing;
+
+    @FindBy (xpath = "//a[@class='nav-link-style'][contains(text(),'Baby Bibs')]")
+    public WebElement babybibs;
+
+    @FindBy (xpath = "//img[@alt='mens-clothing']")
+    public WebElement mensClothingImage;
+
+    @FindBy (xpath = "//div[5]//div[1]//div[1]//h2[1]//a[1]")
+    public WebElement mensClothing;
+
+    //(//h2[@class='h5'][contains(text(),'Men's Clothing')])[5]
+
+    @FindBy (xpath = "(//a[@class='nav-link-style'][contains(text(),'Bottoms')])[1]")
+    public WebElement bottomsM;
+
+
+    @FindBy (xpath = "//img[@alt='womens-clothing']")
+    public WebElement womensClothingImage;
+
+    @FindBy (xpath = "//div[6]//div[1]//div[1]//h2[1]//a[1]")
+    public WebElement womensClothing;
+
+    @FindBy (xpath = "(//a[@class='nav-link-style'][contains(text(),'Bottoms')])[2]")
+    public WebElement bottomsW;
+
+
+
+
+
+
+
+
+}
---
+++ src/test/resources/features/MoreProducts.feature
@@ -0,0 +1,44 @@
+Feature: Verifying the MainPage
+
+  @MoreProducts
+  Scenario: MoreProducts verification
+    Given User is on landing page
+
+    Then User verifies that More products button is present
+    Then User verifies that "accessories" is displayed
+    Then User verifies that "hats" is displayed
+    Then User verifies that "home-living" is displayed
+    Then User verifies that "kids-youth-clothing" is displayed
+    Then User verifies that "mens-clothing" is displayed
+    Then User verifies that "womens-clothing" is displayed
+
+    Then User verifies that "Accessories" heading is present
+    Then User verifies that "Hats" heading is present
+    Then User verifies that "Home & Living" heading is present
+    Then User verifies that "Kids & Youth Clothing" heading is present
+    Then User verifies that "Men's Clothing" heading is present
+    Then User verifies that "Women's Clothing" heading is present
+
+    Then User verifies that "Backpacks" menu is present
+    Then User verifies that "Beanies" menu is present
+    Then User verifies that "Blankets" menu is present
+    Then User verifies that "Baby Bibs" menu is present
+    Then User verifies that "Bottoms" menu is present
+    Then User verifies that "Bottoms" menu is present
+
+#
+#  Scenario: Homepage verification
+#    Given User is on landing page
+#    Then User verifies that "Supply from you print from us" is present
+#    Then User verifies that "Bring Your Own Tee" is present
+#    Then User verifies that Print Now button is present
+#    Then User verifies that "Print picture" is present
+#    Then User verifies that title Brands is present
+#    Then User verifies that Context Custom, GILDAN and BELLA+CANVAS logo under Brands are present
+
+
+
+
+
+
+
