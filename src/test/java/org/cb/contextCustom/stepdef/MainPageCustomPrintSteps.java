3beee7e835381e0e3ded1081942030652f353d9e
ZennT <zenn.diamond@gmail.com>
2021-02-25 15:51:17 -0800

    Created MainPageCustomPrint.feature and MainPageCustomPrintSteps.java

---
+++ src/test/java/org/cb/contextCustom/stepdef/MainPageCustomPrintSteps.java
@@ -0,0 +1,24 @@
+package org.cb.contextCustom.stepdef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MainPageCustomPrintSteps {
+    @Given("User verifies that headings are present")
+    public void userVerifiesThatHeadingsArePresent() {
+    }
+
+    @When("Verifies that {string} button is displayed and enabled")
+    public void verifiesThatButtonIsDisplayedAndEnabled(String arg0) {
+    }
+
+    @Then("Verifies that {string} button is present")
+    public void verifiesThatButtonIsPresent(String arg0) {
+    }
+
+    @And("Verifies that images are present")
+    public void verifiesThatImagesArePresent() {
+    }
+}
---
+++ src/test/resources/features/MainPageCustomPrint.feature
@@ -0,0 +1,7 @@
+Feature:Custom Print Part Functionality
+
+  Scenario:Verifying Custom printed & embroidered products
+    Given User verifies that headings are present
+    When  Verifies that "Make an Order" button is displayed and enabled
+    Then  Verifies that "Print Now" button is present
+    And   Verifies that images are present
